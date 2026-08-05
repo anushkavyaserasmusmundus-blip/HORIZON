b4358ec5db59ac59449405336f3a9ba91b361ad7
anushkavyaserasmusmundus-blip <anushkavyaserasmusmundus@gmail.com>
2026-08-05 15:36:10 +0530

    login

--- backend/horizon/src/main/java/horizon/security/SecurityConfig.java
+++ backend/horizon/src/main/java/horizon/security/SecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
+
 @Configuration
 public class SecurityConfig {
 
@@ -32,6 +37,7 @@ public class SecurityConfig {
         );
 
         http
+            .cors(cors -> {})
             .csrf(csrf -> csrf.disable())
             .httpBasic(basic -> basic.disable())
             .formLogin(form -> form.disable())
@@ -44,5 +50,32 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+public CorsConfigurationSource corsConfigurationSource() {
+
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    configuration.setAllowedOrigins(
+            List.of("http://localhost:5173")
+    );
+
+    configuration.setAllowedMethods(
+            List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
+    );
+
+    configuration.setAllowedHeaders(
+            List.of("*")
+    );
+
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source =
+            new UrlBasedCorsConfigurationSource();
+
+    source.registerCorsConfiguration("/**", configuration);
+
+    return source;
+}
+//CORS is for custom security to overwride browser security. It allows the frontend to make requests to the backend without being blocked by the browser's same-origin policy. In this configuration, we specify the allowed origins, methods, headers, and credentials for cross-origin requests.
     
 }
